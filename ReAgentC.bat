b22a08b5b5566b1012a72589d76fd64f46889d08
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2025-06-14 16:24:58 +0200

    Update ReAgentC.bat

--- ReAgentC.bat
+++ ReAgentC.bat
@@ -1,3 +1,16 @@
 @echo off
 :: Tento software je chráněn licencí „All Rights Reserved“
 :: Používáním souhlasíte s podmínkami uvedenými v LICENSE.md
+color 17
+title ReAgentC
+cls
+echo.
+echo Tento program nalezl chybu a nemuze se spustit spravne
+echo.
+echo Prosim neupravujte tento program a stahnete si overenou verzi programu.
+echo.
+echo Tato cast je jeste vyvijena a v budoucnu budete moct zde resit problemy.
+echo.
+echo Pro pokracovani zmacknete jakoukoliv klavesu....
+pause > nul
+exit
